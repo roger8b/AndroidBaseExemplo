7af71d1ae40e2863bc5d5d574f48d5da5ba968f0
Roger Martins da silva <roger.8b@gmail.com>
2018-08-28 00:33:30 -0300

    Teste de uso dos sensores do android

--- app/src/main/java/com/example/rm/androidbaseexemplo/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/rm/androidbaseexemplo/ui/home/HomeActivity.java
@@ -1,6 +1,16 @@
 package com.example.rm.androidbaseexemplo.ui.home;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -13,10 +23,16 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
+import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Toast;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.blankj.utilcode.util.Utils;
 import com.example.rm.androidbaseexemplo.R;
 import com.example.rm.androidbaseexemplo.databinding.ActivityHomeBinding;
@@ -31,7 +47,7 @@ import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
 public class HomeActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener , SensorEventListener {
 
     private static final String HOME_FRAGMENT = "HOME_FRAGMENT";
     private static final String ROBOT_TWO_FRAGMENT = "ROBOT_TWO_FRAGMENT";
@@ -40,6 +56,13 @@ public class HomeActivity extends BaseActivity
     private Fragment mCurrentFragment;
     private FragmentManager mFragmentManager;
     private Toolbar mToolbar;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private TriggerEventListener mTriggerEventListener;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
 
     private static final long MOVE_DEFAULT_TIME = 300;
     private static final long FADE_DEFAULT_TIME = 300;
@@ -62,8 +85,52 @@ public class HomeActivity extends BaseActivity
 
         loadBottonMenu();
 
-        loadFragment(HOME_FRAGMENT, HomeFragment.newInstance(), R.string.home, null);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
 
+        loadFragment(HOME_FRAGMENT, HomeFragment.newInstance(), R.string.home, null);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 20000;
+
+                if (speed > SHAKE_THRESHOLD) {
+
+                    ToastUtils.showShort("TEste");
+                    LogUtils.log(Log.ASSERT,"x",last_x);
+                    LogUtils.log(Log.ASSERT,"y",last_y);
+                    LogUtils.log(Log.ASSERT,"z",last_z);
+
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+
+
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
@@ -200,6 +267,20 @@ public class HomeActivity extends BaseActivity
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+
+            AnimatorSet mAnimatorSet = new AnimatorSet();
+            mAnimatorSet.setDuration(1000);
+
+
+            ValueAnimator mTranslateAnimator;
+
+            mTranslateAnimator = ObjectAnimator.ofFloat(
+                    mBinding.inContentHome.toolbarOptions,
+                    "translationX",
+                    0, (mBinding.inContentHome.toolbarOptions.getWidth() - mBinding.inContentHome.toolbarOptions.getWidth()));
+            mAnimatorSet.play(mTranslateAnimator).with(mTranslateAnimator);
+            mAnimatorSet.start();
+
             return true;
         }
 
